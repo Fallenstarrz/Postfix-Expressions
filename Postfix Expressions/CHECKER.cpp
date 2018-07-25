572baecbafbb1710ceb880e56dd3aa3584b25084
Christopher Koon <chrkoon@uat.edu>
2018-07-24 22:23:44 -0700

    Finished Assignment

---
+++ Postfix Expressions/CHECKER.cpp
@@ -0,0 +1,16 @@
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	int num1 = 4;
+	int num2 = 2;
+
+	cout << num1 % num2 << endl;
+
+	system("pause");
+	return 0;
+}
